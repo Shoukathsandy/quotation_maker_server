e967de8267c04ec64424363f313475c586841a17
Shoukathsandy <shoukathsandy@gmail.com>
2022-08-31 20:12:17 +0530

    near to flow

--- Routes/Quotation_Maker.js
+++ Routes/Quotation_Maker.js
@@ -1,5 +1,4 @@
 import express from "express";
-import { ObjectId } from "mongodb";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import randomstring from 'randomstring';
@@ -11,7 +10,25 @@ import {
     updateuserpassDetails,
     getUserpasstoken,
     createVendorlist,
-    getvendorlist
+    getvendorlist,
+    updatevendorlist,
+    getvendorbyid,
+    createproductlist,
+    getproductbyidno,
+    getproductlist,
+    updateproductlist,
+    createcustormerlist,
+    getcustomerlist,
+    getcustomerbyid,
+    updatecustomerbyid,
+    getprojectbyid,
+    createprojectlist,
+    getallprojectlist,
+    editprojectbyid,
+    createquotation,
+    getquotationbytid,
+    getallquotation,
+    updatequotation,
 } from "./helper.js";
 
 const router = express.Router();
@@ -31,8 +48,8 @@ router.post("/register", async function (req, res) {
         const existuser = await getusername(email);
 
         if (existuser) {
-
-            res.status(422).send({ error: "already email exist" });
+            console.log(existuser)
+            res.status(422).send({ error: "Already email exist" });
             console.log(existuser.error);
         } else {
             const result = getlogindata({ email: email, password: hashedpassword });
@@ -40,7 +57,7 @@ router.post("/register", async function (req, res) {
         }
     } catch (error) {
         console.log(error);
-        res.status(500).send({ error: "internal server error" });
+        res.status(500).send({ error: "Internal server error" });
     };
 });
 
@@ -54,46 +71,49 @@ router.post("/login", async function (req, res) {
             const storedpassword = existuser.password;
             const ispasswordmatch = await bcrypt.compare(password, storedpassword);
             if (ispasswordmatch) {
+
                 const token = jwt.sign({ id: existuser._id }, process.env.SECRET_KEY);
-                res.send({ msg: "sucessfull login", token: token });
+
+                res.send({ msg: "sucessfull login", token: token, email: email, id: existuser._id });
+
             } else {
-                res.status(401).send({ error: "invalid password or email" });
+                res.status(401).send({ error: "Invalid password or email" });
             }
         }
     } catch (error) {
-        res.status(500).send({ error: "internal server error" });
+        res.status(500).send({ error: "Internal server error" });
     }
 });
 
 router.post("/forgotpassword", async function (req, res) {
-  
+
     try {
-        const {email} = req.body;
+        const { email } = req.body;
         let randomString = randomstring.generate();
         const isuserexist = await getusername(email);
 
         if (!isuserexist) {
-            console.log("invalid email");
-            res.status(401).send({ error: "invalid email" });
+            console.log("invalvendorId email");
+            res.status(401).send({ error: "Invalid email" });
         } else {
-         
-            const linkForUser=`${process.env.FRONTEND_URL}/reset-password/${isuserexist._id}/${randomString}`
-          
+
+            const linkForUser = `${process.env.FRONTEND_URL}/reset-password/${isuserexist._id}/${randomString}`
+
             const transporter = nodemailer.createTransport({
-                service:'gmail',
+                service: 'gmail',
                 auth: {
                     user: process.env.MAIL_USERNAME,
-                  
+
                     pass: process.env.MAIL_PASSWORD,
                 }
-                
+
             });
             //Mail options
             let mailOptions = {
                 from: "no-reply@noreply.com",
-                to:email ,
+                to: email,
                 subject: "Reset Password - BrandFP",
-                html:  `<h4>Hello User,</h4><br><p> You can reset the password by clicking the link below.</p><br><u><a href=${linkForUser}>${linkForUser}</a></u>`,
+                html: `<h4>Hello User,</h4><br><p> You can reset the password by clicking the link below.</p><br><u><a href=${linkForUser}>${linkForUser}</a></u>`,
             };
             //Send mail
             transporter.sendMail(mailOptions, async (err, data) => {
@@ -114,7 +134,7 @@ router.post("/forgotpassword", async function (req, res) {
         }
     } catch (error) {
         console.log(error);
-        res.status(500).send({ error:"internal server error" });
+        res.status(500).send({ error: "Internal server error" });
     }
 });
 // resetpassword method
@@ -128,8 +148,8 @@ router.post("/resetpassword", async function (req, res) {
         // console.log(password);
         // console.log(confirmpassword);
         if (!isuserpasstokenexist) {
-            console.log("invalid msg")
-            res.status(401).send({ error: "invalid credentials" });
+            console.log("invalidvendorId msg")
+            res.status(401).send({ error: "InvalidvendorId credentials" });
         } else {
             if (password === confirmpassword) {
                 const hashpass = await genhashpassword(password);
@@ -142,33 +162,301 @@ router.post("/resetpassword", async function (req, res) {
             }
         }
     } catch (error) {
-        res.status(500).send({ error: "internal error" });
+        res.status(500).send({ error: "Internal error" });
     }
 });
 
 //create vendor list
-router.post("/createvendorlist",async function(req,res){
+router.post("/createvendorlist", async function (req, res) {
     try {
         const data = req.body;
-        const createlist = await createVendorlist(data);
-        console.log(createlist)
-        res.status(200).send(createlist);
+        const Email = req.body.Email;
+        // console.log(Email) 
+        const result = await getvendorbyid(Email);
+        // console.log(result) 
+        if (result) { res.status(401).send({ error: "verndor id already exist" }) }
+        else {
+            const createlist = await createVendorlist(data);
+            // console.log(createlist) 
+            res.status(200).send({ msg: "Successfully Added" })
+        }
+        // res.status(200).send(createlist);
     } catch (error) {
         console.log(error)
-        res.status(500).send({error:"internal server error"})
+        res.status(500).send({ error: "Internal server error" })
     }
 });
 
 //get all vendor list
-router.get("/getvendorlist",async function(req,res){
+router.get("/getvendorlist", async function (req, res) {
     try {
         const result = await getvendorlist();
         res.status(200).send(result);
     } catch (error) {
         console.log(error);
-        res.status(500).send({error:"internal server error"});
+        res.status(500).send({ error: "Internal server error" });
+    }
+});
+
+//get vendorlist by id
+router.get("/getvendorlist/:Email", async function (req, res) {
+    try {
+        const { Email } = req.params;
+        const vendor = await getvendorbyid(Email);
+        console.log(vendor)
+        vendor ? res.send(vendor) : res.send("Email not match");
+
+    } catch (error) {
+        res.status(401).send({ error: "Internal server error" })
+    }
+
+
+})
+
+//edit vendorlist
+router.put("/:Email", async function (req, res) {
+    try {
+        console.log(req.params)
+        const data = req.body;
+        const { Email } = req.params;
+
+        const result = await updatevendorlist(Email, data);
+        res.status(200).send({ msg: "Successfully update" });
+
+    } catch (error) {
+        res.status(401).send({ error: "Internal server error" })
+    }
+
+})
+//create peroductlist
+router.post("/createproductslist", async function (req, res) {
+    try {
+        const data = req.body;
+        const ItemNo = req.body.ItemNo;
+        // console.log(Email) 
+        const result = await getproductbyidno(ItemNo);
+        // console.log(result) 
+        if (result) { res.status(401).send({ error: "Item No already exist" }) }
+        else {
+            const createlist = await createproductlist(data);
+            // console.log(createlist) 
+            res.status(200).send({ msg: "Successfully Added" })
+        }
+        // res.status(200).send(createlist);
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({ error: "Internal server error" })
+    }
+});
+//get productlist
+router.get("/getproductlist", async function (req, res) {
+    try {
+        const data = await getproductlist();
+        res.status(200).send(data);
+    } catch (error) {
+        res.status(401).send({ error: "Internal server error" })
+    }
+})
+//get product by Itemno
+router.get("/getproductlist/:ItemNo", async function (req, res) {
+    try {
+        const { ItemNo } = req.params;
+        const product = await getproductbyidno(ItemNo);
+        console.log(product)
+        product ? res.send(product) : res.send("Email not match");
+
+    } catch (error) {
+        res.status(401).send({ error: "Internal server error" })
+    }
+
+
+})
+//edit productlist 
+router.put("/:ItemNo", async function (req, res) {
+    try {
+        const data = req.body;
+        const { ItemNo } = req.params;
+        const result = await updateproductlist(ItemNo);
+    } catch (error) {
+        res.status(401).send({ error: "Internal server error" })
+    }
+});
+
+//create custormer list
+router.post("/createcustomerlist",async function(req,res){
+    try {
+        const data = req.body;
+        const {Email} = req.body;
+        const result = await getcustomerbyid(Email);
+        if(result){
+            
+            res.status(401).send({msg:"Already Email exist"})
+        }else{
+            const addcustomer = await createcustormerlist(data)
+            res.status(200).send({msg:"Successfully added"})
+        }
+    } catch (error) {
+        res.status(401).send({ error: "Internal server error" })
+    }
+});
+//get all customerlist
+router.get("/getallcustomerlist",async function(req,res){
+    try {
+      
+        const result = await getcustomerlist();
+        if(result){
+        res.status(200).send(result);
+        }else{
+            res.status(401).send({error:"Data not available"})
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(401).send({ error: "Internal server error" })
+    }
+})
+//edit customer by email
+router.put("/updatecustomerdetails/:Email",async function(req,res){
+    try {
+        const data = req.body;
+        const {Email} = req.params;
+        const result = await updatecustomerbyid(Email,data);
+       res.status(200).send({msg:"successfull update"})
+        console.log(result)
+    } catch (error) {
+        console.log(error)
+        res.status(401).send({ error: "Internal server error" })
+    }
+})
+//get customer by id
+router.get("/getcustomerbyemail/:Email",async function(req,res){
+    try {
+        const { Email } = req.params;
+        const result = await getcustomerbyid(Email)
+        console.log(result)
+        result ? res.send(result) : res.status(401).send({error:"Match not found"});
+    } catch (error) {
+        res.status(401).send({ error: "Internal server error" })
     }
 })
 
+//create projectlist
+router.post("/createprojectlist",async function(req,res){
+    try {
+        const data = req.body;
+        const {ProjectId} = req.body;
+        const result = await getprojectbyid(ProjectId)
+        if(result){
+            res.status(401).send({error:"Productlist not created"})
+        }else{
+            const create = await createprojectlist(data);
+            res.status(200).send({msg:"Successfully created"});
+        }
+    } catch (error) {
+        res.status(401).send({ error: "Internal server error" })
+    }
+})
+//get project list
+router.get("/getallprojectlist",async function(req,res){
+    try {
+        const result = await getallprojectlist();
+        if(result){
+            res.status(200).send(result);
+        }else{
+            res.status(401).send({error:"Data not available"})
+        }
+    } catch (error) {
+        res.status(401).send({ error: "Internal server error" })
+    }
+})
+//get project detail by Projectid
+router.get("/getprojectbyid/:ProjectId",async function(req,res){
+    try {
+        const data = req.body;
+        const {ProjectId} = req.params;
+        const result = await getprojectbyid(ProjectId);
+     result ?
+            res.status(200).send(result)
+            :
+            res.status(401).send({error:"Match not found"});
+        
+    } catch (error) {
+        res.status(401).send({ error: "Internal server error" })
+    }
+});
+//update project details by id
+router.put("/updateprojectdetailsbyid/:ProjectNo",async function(req,res){
+    try {
+        const data = req.body;
+        const {ProjectId} = req.params;
+        const result = await editprojectbyid(ProjectId,data);
+        if(result){
+            res.status(200).send({msg:"Successfully updated"})
+        }else{
+            res.status(401).send({error:"Match not found"})
+        }
+    } catch (error) {
+        res.status(401).send({ error: "Internal server error" })
+    }
+});
+// create quotation
+router.post("/createquotation",async function(req,res){
+    try {
+        const data = req.body;
+        const {QuotationId}= req.body;
+        const result = await getquotationbytid(QuotationId)
+        if(result){
+            console.log(result)
+            res.status(401).send({error:"Quotation not created"});
+           
+        }else{
+            const create = await createquotation(data);
+            res.status(200).send({msg:"Create Successfully"})
+        }
+    } catch (error) {
+        res.status(401).send({ error: "Internal server error" })
+    }
+});
+//get all quotation list
+router.get("/getallquotationlist",async function(req,res){
+    try {
+        const result = await getallquotation();
+        if(result){
+            res.status(200).send(result)
+        }else{
+            res.status(401).send({error:"Quotation list not available"})
+        }
+    } catch (error) {
+        res.status(401).send({ error: "Internal server error" })
+    }
+});
+//get quotation by id
+router.get("/getquotationbyid/:QuotationId",async function(req,res){
+    try {
+        const {QuotationId} = req.params;
+        const result = await getquotationbytid(QuotationId);
+        if(result){
+            res.status(200).send(result);
+        }else{
+            res.status(401).send({error:"Match not found"})
+        }
+    } catch (error) {
+        res.status(401).send({ error: "Internal server error" })
+    }
+});
+//update quotation by id
+router.put("/updatequotation/:QuotationId",async function(req,res){
+    try {
+        const data = req.body;
+        const {QuotationId} = req.params;
+        const result = await updatequotation(QuotationId,data);
+        if(result){
+            res.status(200).send({msg:"Successfully added"})
+        }else{
+            res.status(401).send({error:"Not updated"})
+        }
+    } catch (error) {
+        res.status(401).send({ error: "Internal server error" })
+    }
+})
 
 export const Quotation_Maker_Route = router;
--- Routes/helper.js
+++ Routes/helper.js
@@ -34,3 +34,75 @@ export async function createVendorlist(data){
 export async function getvendorlist(){
     return await client.db("quotation").collection("vendorlist").find({}).toArray();
 };
+//get vendorlist by id
+export async function getvendorbyid(Email){
+    return await client.db("quotation").collection("vendorlist").findOne({Email:Email})
+};
+//edit vendorlist
+export async function updatevendorlist(Email,data){
+    return await client.db("quotation").collection("vendorlist").updateOne({Email:Email},{$set:data});
+}
+//create productlist 
+export async function createproductlist(data){
+    return await client.db("quotation").collection("productlist").insertOne(data)
+}
+//get productlist by item no
+export async function getproductbyidno(ItemNo){
+    return await client.db("quotation").collection("productlist").findOne({ItemNo:ItemNo})
+}
+//get all productlist
+export async  function getproductlist(){
+    return await client.db("quotation").collection("productlist").find({}).toArray();
+}
+//edit productlist
+export async function updateproductlist(ItemNo,data){
+    return await client.db("quotation").collection("productlist").updateOne({ItemNo},{$set:data});
+}
+//crreate customerslist
+export  async function createcustormerlist(data){
+    return await client.db("quotation").collection("customerlist").insertOne(data);
+}
+
+//get all customerslist
+export async function getcustomerlist(){
+    return await client.db("quotation").collection("customerlist").find({}).toArray();
+};
+//get customerlist by email
+export async function getcustomerbyid(Email){
+    return await client.db("quotation").collection("customerlist").findOne({Email:Email});
+}//edit customer details
+export async function updatecustomerbyid(Email,data){
+    return await client.db("quotation").collection("customerlist").updateOne({Email:Email},{$set:data})
+}
+// create project list
+export async function createprojectlist(data){
+    return await client.db("quotation").collection("projectlist").insertOne(data);
+};
+//get all projectlist
+export async function getallprojectlist(){
+    return await client.db("quotation").collection("projectlist").find({}).toArray();
+};
+// get project by p.no
+export async function getprojectbyid(ProjectId){
+    return await client.db("quotation").collection("projectlist").findOne({ProjectId:ProjectId});
+};
+// edit project byt p.no
+export async function editprojectbyid(ProjectId,data){
+    return await client.db("quotation").collection("projectlist").updateOne({ProjectId:ProjectId},{$set:data});
+};
+//create quotation
+export async function createquotation(data){
+    return await client.db("quotation").collection("quotationlist").insertOne(data);
+};
+// get all quotation 
+export async function getallquotation(){
+    return await client.db("quotation").collection("quotationlist").find({}).toArray();
+};
+// get quotation by quotation id
+export async function getquotationbytid(QuotationId){
+    return await client.db("quotation").collection("quotationlist").findOne({QuotationId:QuotationId});
+};
+// update quotation 
+export async function updatequotation(QuotationId,data){
+    return await client.db("quotation").collection("quotationlist").updateOne({QuotationId},{$set:data})
+}
